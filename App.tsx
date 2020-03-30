b2dd5248dbd91ce37f1c7eade0a620740ad9a22a
Ryutaro0802 <ne200152g@gmail.com>
2020-03-31 01:12:38 +0900

    フォントの読み込み

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
-import { AppLoading } from 'expo';
-import { View, StyleSheet } from 'react-native';
-import { Container, Content, Form, Item, Input, Textarea, Label, Text } from 'native-base';
-import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
+import React, { useState } from 'react'
+import { AppLoading } from 'expo'
+import { View, StyleSheet } from 'react-native'
+import { Container, Content, Form, Item, Input, Textarea, Label, Text, Button } from 'native-base'
+import * as Font from 'expo-font'
+import { Ionicons } from '@expo/vector-icons'
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false)
+  const loadAssets = async () => {
+    await Font.loadAsync({
+      Roboto: require('./node_modules/native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('./node_modules/native-base/Fonts/Roboto_medium.ttf'),
+    })
+  }
+  const press = () => {
+    console.log('onPress!')
+  }
+  if (!isReady) {
+    return <AppLoading
+      startAsync={loadAssets}
+      onFinish={() => setIsReady(true)}
+    />
+  }
   return (
     <Container style={styles.container}>
       <Content>
@@ -15,10 +31,16 @@ export default function App() {
             <Input />
           </Item>
           <Item inlineLabel>
-            <Label>Username</Label>
+            <Label>Time</Label>
             <Input />
           </Item>
-          {/* <Textarea rowSpan={5} bordered placeholder="Textarea" /> */}
+          <View style={styles.textAreaContainer}>
+            <Textarea rowSpan={5} underline bordered placeholder="Memo" />
+          </View>
+
+          <Button onPress={press}>
+            <Text>Click Me!</Text>
+          </Button>
         </Form>
       </Content>
     </Container>
@@ -29,5 +51,9 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: 30,
     paddingBottom: 30,
-  }
+  },
+  textAreaContainer: {
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
 })
