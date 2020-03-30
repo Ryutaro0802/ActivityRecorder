46044277c8e0921502d19dbf23236f7070dde698
Ryutaro0802 <ne200152g@gmail.com>
2020-03-31 00:35:07 +0900

    スタイル追加

--- App.tsx
+++ App.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import { AppLoading } from 'expo';
-import { Container, Text } from 'native-base';
+import { View, StyleSheet } from 'react-native';
+import { Container, Content, Form, Item, Input, Textarea, Label, Text } from 'native-base';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <Container>
-        <Text>Open up App.js to start working on your app!</Text>
-      </Container>
-    );
-  }
+export default function App() {
+  return (
+    <Container style={styles.container}>
+      <Content>
+        <Form>
+          <Item inlineLabel>
+            <Label>Title</Label>
+            <Input />
+          </Item>
+          <Item inlineLabel>
+            <Label>Username</Label>
+            <Input />
+          </Item>
+          {/* <Textarea rowSpan={5} bordered placeholder="Textarea" /> */}
+        </Form>
+      </Content>
+    </Container>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 30,
+    paddingBottom: 30,
+  }
+})
