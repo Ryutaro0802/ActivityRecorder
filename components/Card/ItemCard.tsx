cd7b9d20659b0058346c161693936fb9ac36d03f
Ryutaro0802 <ne200152g@gmail.com>
2020-04-28 00:56:00 +0900

    FormComponent実装

--- components/Card/ItemCard.tsx
+++ components/Card/ItemCard.tsx
@@ -1,29 +1,30 @@
 import React, { FC } from 'react'
 import { Text, Card, CardItem, Body } from 'native-base'
-import { ShadowPropTypesIOS } from 'react-native'
 
 interface ItemCardProps {
     title: string
     body: string
 }
 
-const ItemCard: FC<ItemCardProps> = props => (
-    <>
-        <Card>
-            <CardItem header>
-                <Text>
-                    {props.title}
-                </Text>
-            </CardItem>
-            <CardItem>
-                <Body>
+const ItemCard: FC<ItemCardProps> = ({
+    title, body
+}) => (
+        <>
+            <Card>
+                <CardItem header>
                     <Text>
-                        {props.body}
+                        {title}
                     </Text>
-                </Body>
-            </CardItem>
-        </Card>
-    </>
-)
+                </CardItem>
+                <CardItem>
+                    <Body>
+                        <Text>
+                            {body}
+                        </Text>
+                    </Body>
+                </CardItem>
+            </Card>
+        </>
+    )
 
 export default ItemCard
