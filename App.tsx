6008129c15b800256fc34682648b0afbb7ffe294
Ryutaro0802 <ne200152g@gmail.com>
2020-05-15 00:42:40 +0900

    StackScreen実装

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
+import "react-native-gesture-handler";
 import React, { useState } from "react";
 import { AppLoading } from "expo";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import { Container, Content } from "native-base";
 import * as Font from "expo-font";
 // import { Ionicons } from '@expo/vector-icons'
@@ -9,12 +10,26 @@ import ActivityItemList from "./src/containers/ActivityItemList";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ListScreen from "./src/components/Screen/ListScreen";
 
 import activityItemReducer from "./src/services/activityItems/reducers";
 import rootSaga from "./src/services/activityItems/saga";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 const sagaMiddleWare = createSagaMiddleware();
 const store = createStore(activityItemReducer, applyMiddleware(sagaMiddleWare));
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
 
 export default function App() {
   const [isReady, setIsReady] = useState(false);
@@ -31,17 +46,22 @@ export default function App() {
     );
   }
   return (
-    <Provider store={store}>
-      <Container style={styles.container}>
-        <Content>
-          <ActivityItemForm />
-        </Content>
+    // <Provider store={store}>
+    //   <Container style={styles.container}>
+    //     <Content>
+    //       <ActivityItemForm />
+    //     </Content>
 
-        <Content>
-          <ActivityItemList />
-        </Content>
-      </Container>
-    </Provider>
+    //     <Content>
+    //       <ActivityItemList />
+    //     </Content>
+    //   </Container>
+    // </Provider>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/components/Screen/ListScreen.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+import ActivityItemList from "./src/containers/ActivityItemList";
+import { View, Text } from "react-native";
+
+function ListScreen() {
+  return (
+    <View>
+      <Text>ListScreen</Text>
+    </View>
+  );
+}
+
+export default ListScreen;
