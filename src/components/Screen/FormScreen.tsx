ff3095df3c7048ba16ae69f8ed4b99bedec0587c
Ryutaro0802 <ne200152g@gmail.com>
2020-05-17 01:06:06 +0900

    Headerを追加

--- src/components/Screen/FormScreen.tsx
+++ src/components/Screen/FormScreen.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { Container, Header, Body, Title } from "native-base";
 import ActivityItemForm from "../../containers/ActivityItemForm";
 
 function FormScreen() {
-  return <ActivityItemForm />;
+  return (
+    <Container>
+      <Header>
+        <Body>
+          <Title>Form</Title>
+        </Body>
+      </Header>
+      <ActivityItemForm />
+    </Container>
+  );
 }
 
 export default FormScreen;
--- src/components/Screen/ListScreen.tsx
+++ src/components/Screen/ListScreen.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { Container, Header, Body, Title } from "native-base";
 import ActivityItemList from "../../containers/ActivityItemList";
 
 function ListScreen() {
-  return <ActivityItemList />;
+  return (
+    <Container>
+      <Header>
+        <Body>
+          <Title>List</Title>
+        </Body>
+      </Header>
+      <ActivityItemList />
+    </Container>
+  );
 }
 
 export default ListScreen;
